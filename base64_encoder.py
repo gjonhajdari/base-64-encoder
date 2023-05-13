69e76475e72deb9c1371ce88e29d200cc35c0103
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-05-13 20:02:17 +0200

    Created project structure

---
+++ base64_encoder.py
@@ -0,0 +1,14 @@
+class Base64:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.binary_data = self.read_contents()
+
+    def read_contents(self):
+        return
+
+    def encode(self):
+        return
+
+    def decode(self, encoded_string):
+        return
