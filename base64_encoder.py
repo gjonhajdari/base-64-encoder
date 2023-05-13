5bcdb31e66815e25bb2bf66168b628d0f51bd1ea
hekurani <121341921+hekurani@users.noreply.github.com>
2023-05-13 20:12:44 +0200

    Update base64_encoder.py
    encode function in base64

--- base64_encoder.py
+++ base64_encoder.py
@@ -8,7 +8,17 @@ class Base64:
         return
 
     def encode(self):
-        return
+        # Store the binary string into an array with strings that are 6 characters long
+        binary_chunks = [self.binary_data[i:i + 6] for i in range(0, len(self.binary_data), 6)]
+        encoded_string = ""
+        for binary_string in binary_chunks:
+            if len(binary_string) != 6:
+                continue
+            else:
+                # Map the binary chunk to an ASCII character
+                encoded_string += bin_to_character_table[binary_string]
+
+        return encoded_string
 
     def decode(self, encoded_string):
         return
